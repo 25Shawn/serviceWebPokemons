de3cf4631cbb43e61928d9c30596e8bf6aba7d73
25Shawn <shawndutil@icloud.com>
2024-03-14 10:01:29 -0400

    test7

--- src/controllers/controllerPokemons.js
+++ src/controllers/controllerPokemons.js
@@ -7,7 +7,7 @@ exports.AfficherUnPokemon = (req, res) => {
         .then(resultat => {
             if (resultat.length === 0) {
                 return res.status(404).send("Pokemon introuvable avec l'id ${params}");
-                
+
             } else {
                 return res.status(200).send({
                     nom: resultat[0].nom,
@@ -35,7 +35,19 @@ exports.AfficherListePokemons = (req, res) => {
             if (resultat.length === 0) {
                return res.status(404).send(`Aucun pokemon de type ${req.query.type} trouvé`);
             } else {
-               return res.send(resultat);
+               return res.status(200).send({
+                pokemon: [
+
+                    {
+                        nom: resultat[0].nom,
+                        type_primaire: resultat[0].type_primaire,
+                        type_secondaire: resultat[0].type_secondaire,
+                        pv: resultat[0].pv,
+                        attaque: resultat[0].attaque,
+                        defense: resultat[0].defense
+                    }
+                ]
+                });
             }
         })
         .catch(erreur => {
@@ -56,11 +68,24 @@ exports.AjouterUnPokemon = (req, res) => {
                 return res.status(404).send("Le pokemon id ${req.params.id} n'a pas été ajouté");
             }
             else {
-                return res.status(201).send("Le pokemon ${req.params.nom} a été ajouté avec succès");
+                return res.status(201).send(
+                    {
+                        message : "Le pokemon ${req.params.nom} a été ajouté avec succès",
+                        pokemon : {
+                            id: resultat.insertId,
+                            nom: req.query.nom,
+                            type_primaire: req.query.type_primaire,
+                            type_secondaire: req.query.type_secondaire,
+                            pv: req.params.pv,
+                            attaque: req.params.attaque,
+                            defense: req.params.defense
+                        }
+                    }
+
+                );
             }
         })
         .catch(erreur => {
-            console.log("Erreur: ", erreur);
             return res.status(500).send("Echec lors de la création du pokemon ${req.params.nom}");
         });
         
